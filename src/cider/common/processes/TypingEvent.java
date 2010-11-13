7b176a389bd4758b59ab73d2f8b31e7cf3c4bff9
Pikmindoctor <Pikmindoctor@gmail.com>
2010-11-13 17:17:17 +0000

    re-wrote a lot of the concurrent editing code. This time with actual sanity.

--- src/cider/common/processes/TypingEvent.java
+++ src/cider/common/processes/TypingEvent.java
@@ -10,21 +10,21 @@ public class TypingEvent
     public char chr;
     public long time;
     public TypingEventMode mode;
-    public double eventNumber; // only set if caret position changed
+    public int caretPosition; // only set if caret position changed
 
     public TypingEvent(long time, TypingEventMode mode, int caretPosition,
             char chr)
     {
         this.time = time;
         this.mode = mode;
-        this.eventNumber = caretPosition;
+        this.caretPosition = caretPosition;
         this.chr = chr;
     }
 
     @Override
     public String toString()
     {
-        return this.time + "\t" + this.eventNumber + "\t" + mode.toString()
+        return this.time + "\t" + this.caretPosition + "\t" + mode.toString()
                 + " " + chr;
     }
 }
