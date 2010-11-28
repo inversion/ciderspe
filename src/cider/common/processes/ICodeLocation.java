70d285218ccec7e8e3324f2ce239ee5bc26e97e5
Pikmindoctor <Pikmindoctor@gmail.com>
2010-11-28 18:51:05 +0000

    Attempt at merging existing repository with Andrews...

--- src/cider/common/processes/ICodeLocation.java
+++ src/cider/common/processes/ICodeLocation.java
@@ -4,11 +4,9 @@ import java.util.Queue;
 
 public interface ICodeLocation
 {
-    public void setOpen(boolean open);
-
-    public boolean isOpen();
-
     public void push(Queue<TypingEvent> typingEvents);
 
-    public Queue<TypingEvent> update();
+    public Queue<TypingEvent> events();
+
+    public Queue<TypingEvent> eventsSince(long time);
 }
--- src/cider/common/processes/TypingEvent.java
+++ src/cider/common/processes/TypingEvent.java
@@ -48,17 +48,30 @@ public class TypingEvent
                 + this.time;
     }
 
-    public TreeMap<Double, TypingEvent> explode(final double keyRange)
+    public TreeMap<Double, TypingEvent> explode()
+    {
+        int len = this.text.length();
+        return this.explode(len > 0 ? 1 : len);
+    }
+
+    public TreeMap<Double, TypingEvent> explode(final double amountOfTime)
     {
         TreeMap<Double, TypingEvent> result = new TreeMap<Double, TypingEvent>();
-        final double inc = keyRange / this.text.length();
-        int pos = 0;
-        double t;
-        for (char c : this.text.toCharArray())
+        int len = this.text.length();
+        if (len == 0)
+            result.put((double) this.time, this);
+        else
         {
-            pos++;
-            t = this.time + inc * pos;
-            result.put(t, new TypingEvent(this.time, this.mode, pos, "" + c));
+            final double inc = amountOfTime / this.text.length();
+            int pos = 0;
+            double t;
+            for (char c : this.text.toCharArray())
+            {
+                pos++;
+                t = this.time + inc * pos;
+                result.put(t,
+                        new TypingEvent(this.time, this.mode, pos, "" + c));
+            }
         }
         return result;
     }
