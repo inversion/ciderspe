03896964b3c2bb2aecaffd5fef89f24618b38de7
Pikmindoctor <Pikmindoctor@gmail.com>
2010-12-05 12:20:32 +0000

    refractored tree stuff to DirectoryViewComponent

--- src/cider/client/gui/DirectoryViewComponent.java
+++ src/cider/client/gui/DirectoryViewComponent.java
@@ -3,10 +3,11 @@ package cider.client.gui;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeModel;
 
 public class DirectoryViewComponent extends JPanel
 {
@@ -20,14 +21,57 @@ public class DirectoryViewComponent extends JPanel
         w.setVisible(true);
     }
 
-    TreeModel model;
-    DefaultMutableTreeNode top = new DefaultMutableTreeNode("Root");
-    JTree tree = new JTree(this.top);
-
     public DirectoryViewComponent()
     {
-        this.add(this.tree);
-        this.model = this.tree.getModel();
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode(
+                "Some root folder");
+        createNodes(top);
+
+        JTree tree = new JTree(top);
+        // tree.setModel(model);
+
+        JScrollPane scrollpane = new JScrollPane(tree);
+
+        // split pane
+        JLabel label;
+        label = new JLabel("directory tree");
+
+        JLabel label2;
+        label2 = new JLabel("code");
+
+        JLabel label3;
+        label3 = new JLabel("chat, oh hai");
+
+        this.add(scrollpane);
+    }
+
+    public void createNodes(DefaultMutableTreeNode top)
+    {
+        /* Creates some objects to simulate files and folders etc */
+        DefaultMutableTreeNode category = null;
+        DefaultMutableTreeNode book = null;
+
+        category = new DefaultMutableTreeNode("Folder 1");
+        top.add(category);
+
+        // book = new DefaultMutableTreeNode(new BookInfo
+        // ("The Java Tutorial: A Short Course on the Basics",
+        // "tutorial.html"));
+        for (int i = 0; i < 50; i++)
+        {
+            book = new DefaultMutableTreeNode("File 1");
+            category.add(book);
+        }
+        /*
+         * DefaultMutableTreeNode sub = null; sub = new
+         * DefaultMutableTreeNode("SubFile 1"); book.add(sub);
+         */
+
+        category = new DefaultMutableTreeNode("Folder 2");
+        top.add(category);
+
+        book = new DefaultMutableTreeNode("File 2");
+        category.add(book);
     }
 
     public void parseXML(String xml)
