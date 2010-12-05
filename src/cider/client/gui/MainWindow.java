a68d3fcb2c59e4be1b682c0598c7820d76e12c30
jonnyb12345 <jonnyb12345@gmail.com>
2010-12-05 14:38:50 +0000

    I added ActionListeners and KeyListeners to the buttons to add functionality The DEV menu was added to help with testing A small error still exists when saving; I'll fix this later today Any questions, ask me on skype :)

--- src/cider/client/gui/MainWindow.java
+++ src/cider/client/gui/MainWindow.java
@@ -4,7 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
@@ -19,26 +25,110 @@ import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 class MainWindow implements Runnable
 {
     JFrame w;
-
+    public String currentFileName = "newfile.java";
+    public String currentFileContents;
+    
     public static void main(String[] args)
     {
         MainWindow program = new MainWindow();
         SwingUtilities.invokeLater(program);
     }
 
-    public static void addMenuItem(JMenu menu, String name, int keyEvent)
+    public static void addMenuItem(JMenu menu, String name, int keyEvent, ActionListener a)
     {
         JMenuItem menuItem = new JMenuItem(name);
+        menuItem.addActionListener(a);
         if (keyEvent != -1)
             menuItem.setAccelerator(KeyStroke.getKeyStroke(keyEvent,
                     ActionEvent.CTRL_MASK));
         menu.add(menuItem);
+    }
+    
+    public ActionListener newAction()
+    {
+    	ActionListener AL = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String action = e.getActionCommand();
+				if (action.equals("Quit"))
+				{
+					int response;
+		            response = JOptionPane.showConfirmDialog(null, "Are you sure you wish to quit without saving?");
+		            if (response == 0)
+		            	System.exit(0);
+					
+				}
+				else if (action.equals("Save file locally") || action.equals("Save") || action.equals("Save As"))
+				{
+					if (currentFileName.equals("newfile.java") || action.equals("Save As"))
+					{
+						String response = JOptionPane.showInputDialog(null,
+							  "Enter new file name",
+							  currentFileName,
+							  JOptionPane.QUESTION_MESSAGE);
+						currentFileName = response;
+					}
+		            File f = new File (currentFileName);
+		            if (!f.exists())
+		            {
+						try 
+						{
+							f.createNewFile();
+				            FileWriter fstream = new FileWriter(currentFileName);
+				            BufferedWriter out = new BufferedWriter(fstream);
+				            out.write(currentFileContents);
+				            out.close();
+						} 
+						catch (IOException e1) 
+						{
+							e1.printStackTrace();
+						}
+		            }
+				}
+
+			}
+    	};
+    	return AL;
+    }
+    
+    public KeyListener newKeyListener()
+    {
+    	KeyListener k;
+    	k = new KeyListener(){
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int key = e.getKeyCode();
+				if (key == KeyEvent.VK_ENTER)
+				{
+					JTextArea temp = (JTextArea) e.getSource();
+					currentFileContents = temp.getText();
+					System.out.println(currentFileContents);
+				}		
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+    		
+    	};
+    	return k;
     }
 
     public JMenuBar mainMenuBar()
@@ -49,26 +139,40 @@ class MainWindow implements Runnable
         // menu 1
         menu = new JMenu("File");
         menuBar.add(menu);
+        
+        ActionListener aL = newAction();
 
-        addMenuItem(menu, "New", KeyEvent.VK_N);
-        addMenuItem(menu, "Open", KeyEvent.VK_O);
-        addMenuItem(menu, "Save", KeyEvent.VK_S);
-        addMenuItem(menu, "Save As", -1);
-        addMenuItem(menu, "Quit", KeyEvent.VK_Q);
+        addMenuItem(menu, "New", KeyEvent.VK_N, aL);
+        addMenuItem(menu, "Open", KeyEvent.VK_O, aL);
+        addMenuItem(menu, "Save", KeyEvent.VK_S, aL);
+        addMenuItem(menu, "Save As", -1, aL);
+        addMenuItem(menu, "Quit", KeyEvent.VK_Q, aL);
 
         // menu 2
         menu = new JMenu("Edit");
         menuBar.add(menu);
 
-        addMenuItem(menu, "Cut", KeyEvent.VK_X);
-        addMenuItem(menu, "Copy", KeyEvent.VK_C);
-        addMenuItem(menu, "Paste", KeyEvent.VK_V);
+        addMenuItem(menu, "Cut", KeyEvent.VK_X, aL);
+        addMenuItem(menu, "Copy", KeyEvent.VK_C, aL);
+        addMenuItem(menu, "Paste", KeyEvent.VK_V, aL);
 
         // menu 3
         menu = new JMenu("Help");
         menuBar.add(menu);
 
-        addMenuItem(menu, "About", -1);
+        addMenuItem(menu, "About", -1, aL);
+        
+        //the DEV(eloper) menu is for us to test back-end things such as saving and pushing
+        //NYI = not yet implemented
+        menu = new JMenu("DEV");
+        menuBar.add(menu);
+        
+        addMenuItem(menu, "Save file locally", -1, aL);
+        addMenuItem(menu, "Push file to server (NYI)", -1, aL);
+        addMenuItem(menu, "Get file list from server (NYI)", -1, aL);
+        addMenuItem(menu, "Pull item from server (NYI)", -1, aL);
+        
+        
 
         return menuBar;
     }
@@ -77,12 +181,12 @@ class MainWindow implements Runnable
     {
         JPanel panel = new JPanel(new BorderLayout());
         // text area
-        JTextArea textArea;
-        textArea = new JTextArea();
+        JTextArea textArea = new JTextArea(
+        		"class Hello{\n\tpublic static void main(String[] args) \n\t{\n\t\tSystem.out.println(\"hello\");\n\t}\n}");
         JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane
-                .setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setPreferredSize(new Dimension(250, 250));
+        textArea.addKeyListener(newKeyListener());
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         panel.add(scrollPane);
