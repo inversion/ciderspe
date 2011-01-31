1f284b9e3aa5b0d3c16ea35d47c2fe1fb0e9cc63
alexsheppard11 <alexsheppard11@gmail.com>
2011-01-31 14:40:23 +0000

    tidy encryption file

--- src/cider/client/gui/passwordEncrypt.java
+++ src/cider/client/gui/passwordEncrypt.java
@@ -7,31 +7,30 @@ package cider.client.gui;
 public class passwordEncrypt
 {
 	public static String encrypt(String input)
-	{
-		System.out.println(input);
-		
-		String encryptedText = "";
+	{		
+		String output = "";
 		for (int i = 0; i < input.length(); i++)
 		{
 			char c = input.charAt(i);
 			c = (char) (c + 128);
-			encryptedText+= c;
+			output+= c;
 		}
-		System.out.println(encryptedText);
-		return encryptedText;
+		System.out.println("Input : " + input);
+		System.out.println("Output: " + output);
+		return output;
 	}
 	
 	public static String decrypt(String input)
 	{
-		String encryptedText = "";
+		String output = "";
 		for (int i = 0; i < input.length(); i++)
 		{
 			char c = input.charAt(i);
 			c = (char) (c - 128);
-			encryptedText+= c;
+			output+= c;
 		}
 		System.out.println("Input : " + input);
-		System.out.println("Output: " + encryptedText);
-		return encryptedText;
+		System.out.println("Output: " + output);
+		return output;
 	}
 }
