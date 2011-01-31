f703c66046e539e5f15af773305d52a8499fcee9
mossy1278 <mossy1278@gmail.com>
2011-01-31 11:54:51 +0000

    Multi user chat working, but rather unrefined as of yet (prints out presence stanzas etc.), had to use nightly build of smack libraries to work around a bug in them. Had to work around google obtuseness by making the bot start the chatroom and invite everyone else to it when they start a private chat with the bot. You can't join google chatrooms without an invite.

---
+++ src/cider/common/network/ClientChatroomMessageListener.java
@@ -0,0 +1,24 @@
+package cider.common.network;
+
+import java.util.Date;
+
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+
+public class ClientChatroomMessageListener implements PacketListener {
+
+	private Client client;
+
+	public ClientChatroomMessageListener( Client source ) {
+		client = source;
+	}
+
+	@Override
+	public void processPacket(Packet packet) {
+		// TODO Auto-generated method stub
+		Message msg = (Message) packet;
+		client.updateChatLog( msg.getFrom(), new Date(), msg.getBody() );
+	}
+
+}
