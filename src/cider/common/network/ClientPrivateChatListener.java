9a2b5050c496779205eceac4939f44921b637f54
Pikmindoctor <Pikmindoctor@gmail.com>
2011-02-02 16:24:59 +0000

    Line locking - it actually seems to work now. Although really you're locking regions, so as well as lines you could select a bunch of text and lock that.

---
+++ src/cider/common/network/ClientPrivateChatListener.java
@@ -0,0 +1,37 @@
+package cider.common.network;
+
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+
+/**
+ * 
+ * Listen for newly created user chats and assign them a message listener.
+ * 
+ * @author Andrew
+ *
+ */
+
+public class ClientPrivateChatListener implements ChatManagerListener {
+	
+	// TODO: Not sure if there's any point in this chats array
+	private ArrayList<Chat> chats;
+	private ClientPrivateChatMessageListener userChatMessageListener;
+
+	ClientPrivateChatListener(DefaultListModel userListModel)
+	{
+		chats = new ArrayList<Chat>();
+		userChatMessageListener = new ClientPrivateChatMessageListener();
+	}
+	
+	@Override
+	public void chatCreated(Chat chat, boolean createdLocally) {
+		// TODO Auto-generated method stub
+		chats.add( chat );
+		chat.addMessageListener( userChatMessageListener );
+	}
+
+}
