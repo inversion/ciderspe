3ce1e2ef4da5f43421c3b15bc7b29fea37de982e
Pikmindoctor <Pikmindoctor@gmail.com>
2011-02-13 13:53:27 +0000

    BotChatroomMessageListener is now being used to log typing events in the bots source document (instead of clients sending typing events to the bot directly)

--- src/cider/common/network/BotChatroomMessageListener.java
+++ src/cider/common/network/BotChatroomMessageListener.java
@@ -7,15 +7,44 @@ package cider.common.network;
  * 
  */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Packet;
 
-public class BotChatroomMessageListener implements PacketListener {
+import cider.common.processes.TypingEvent;
 
-	@Override
-	public void processPacket(Packet packet) {
-		// TODO Auto-generated method stub
+public class BotChatroomMessageListener implements PacketListener
+{
 
-	}
+    private Bot bot;
 
+    public BotChatroomMessageListener(Bot bot)
+    {
+        this.bot = bot;
+    }
+
+    @Override
+    public void processPacket(Packet packet)
+    {
+        Message msg = (Message) packet;
+        String body = msg.getBody();
+        if (body.startsWith("pushto("))
+        {
+            String[] instructions = body.split("\\n");
+            for (String instruction : instructions)
+            {
+                String[] preAndAfter = instruction.split("\\) ");
+                String[] pre = preAndAfter[0].split("\\(");
+                String dest = pre[1];
+                dest = dest.replace("root\\", "");
+                Queue<TypingEvent> typingEvents = new LinkedList<TypingEvent>();
+                typingEvents.add(new TypingEvent(preAndAfter[1]));
+                System.out.println("Push " + preAndAfter[1] + " to " + dest);
+                this.bot.getRootFolder().path(dest).push(typingEvents);
+            }
+        }
+    }
 }
