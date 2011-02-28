803268ba5cf6abf5ec29d2c811e9cd9504811d45
Pikmindoctor <Pikmindoctor@gmail.com>
2011-02-28 16:51:46 +0000

    LockingRegion has been deprecated (I never used it in the end)

--- src/cider/common/processes/LockingRegion.java
+++ src/cider/common/processes/LockingRegion.java
@@ -1,16 +1,19 @@
 package cider.common.processes;
 
+@Deprecated
 public class LockingRegion implements Comparable<LockingRegion>
 {
     public String owner;
     public int start;
     public int end;
 
+    @Deprecated
     public LockingRegion(int start)
     {
         this.start = start;
     }
 
+    @Deprecated
     public LockingRegion(String owner, int start, int end)
     {
         this.owner = owner;
@@ -18,12 +21,14 @@ public class LockingRegion implements Comparable<LockingRegion>
         this.end = end;
     }
 
+    @Deprecated
     public boolean coversOver(String self, int position)
     {
         return position >= this.start && position <= this.end;// &&
         // !this.owner.equals(self);
     }
 
+    @Deprecated
     public void move(int amount)
     {
         this.start += amount;
@@ -31,6 +36,7 @@ public class LockingRegion implements Comparable<LockingRegion>
     }
 
     @Override
+    @Deprecated
     public int compareTo(LockingRegion lr)
     {
         if (this.start < lr.start)
