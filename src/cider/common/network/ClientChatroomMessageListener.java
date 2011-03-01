d45be47159e340b08b5fb03b046d52544669a52c
alexsheppard11 <alexsheppard11@gmail.com>
2011-03-01 23:36:48 +0000

    new/import

---
+++ src/cider/common/network/ClientChatroomMessageListener.java
@@ -0,0 +1,55 @@
+package cider.common.network;
+
+import java.io.IOException;
+
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+
+import cider.specialcomponents.Base64;
+
+/**
+ * Listens for new messages in chatrooms.
+ * 
+ * (Currently there is only one room, created by the bot, for all clients).
+ * 
+ * @author Andrew
+ * 
+ */
+
+public class ClientChatroomMessageListener implements PacketListener
+{
+
+    private Client client;
+
+    public ClientChatroomMessageListener(Client source)
+    {
+        client = source;
+    }
+
+    @Override
+    public void processPacket(Packet packet)
+    {
+        // TODO Auto-generated method stub
+        Message msg = (Message) packet;
+        if (msg.getType() == Message.Type.groupchat)
+        {
+            String body = null;
+            try
+            {
+                body = new String(Base64.decode(msg.getBody()));
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block=
+                e.printStackTrace();
+            }
+            if (body.startsWith("filelist=") || body.startsWith("pushto(")
+                    || body.startsWith("empty"))
+                client.processDocumentMessages(body);
+            else
+                client.updateChatLog(msg.getFrom(), msg.getSubject(), body);
+        }
+    }
+
+}
---
+++ src/cider/common/processes/SourceDocumentDemo.java
@@ -0,0 +1,365 @@
+package cider.common.processes;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import cider.specialcomponents.EditorTypingArea;
+
+/**
+ * @author Lawrence
+ * 
+ */
+public class SourceDocumentDemo
+{
+    private PseudoServer server = new PseudoServer();
+
+    public static void main(String[] args)
+    {
+        SourceDocumentDemo app = new SourceDocumentDemo();
+        app.openInstance(1);
+        app.openInstance(2);
+        app.openInstance(3);
+    }
+
+    public void openInstance(int id)
+    {
+        JFrame w = new JFrame();
+        w.setSize(640, 480);
+        w.setLocationByPlatform(true);
+        int offset = (id - 1) * 100;
+        w.setLocation(w.getX() + offset, w.getY() + offset);
+        SourceDocument sourceDocument = new SourceDocument("Demo User " + id,
+                "test.SourceDocument");
+        EditorTypingArea eta = new EditorTypingArea(sourceDocument.getOwner(),
+                sourceDocument);
+        eta.setWaiting(false);
+        SDDemoPanel panel = new SDDemoPanel(w, w.getSize(), this.server,
+                this.server.timer, id, eta);
+        w.add(panel);
+        w.pack();
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        w.setVisible(true);
+        PseudoClient client = new PseudoClient(panel, id, sourceDocument);
+        this.server.addClient(client);
+        panel.setClient(client);
+    }
+
+    public class PseudoServer implements ICodeLocation
+    {
+        private SourceDocument sourceDocument = new SourceDocument("TestBot",
+                "Bot Document");
+        private ArrayList<ICodeLocation> clients = new ArrayList<ICodeLocation>();
+        private Timer timer = new Timer();
+        private long delay = 1;
+        private long period = 1000;
+        private long currentTime = System.currentTimeMillis();
+        private long lastPush = 0;
+
+        public PseudoServer()
+        {
+            this.timer.scheduleAtFixedRate(new TimerTask()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        currentTime = System.currentTimeMillis();
+                        // System.out.println(currentTime);
+                        Queue<TypingEvent> recentEvents = sourceDocument
+                                .eventsSince(lastPush);
+
+                        if (recentEvents != null && recentEvents.size() > 0)
+                        {
+                            for (ICodeLocation client : clients)
+                            {
+                                Queue<TypingEvent> pushQueue = new LinkedList<TypingEvent>();
+                                pushQueue.addAll(recentEvents);
+                                client.push(pushQueue);
+                            }
+                        }
+                        lastPush = currentTime - 1;
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+                }
+            }, this.delay, this.period);
+        }
+
+        public void addClient(ICodeLocation client)
+        {
+            this.clients.add(client);
+        }
+
+        @Override
+        public void push(Queue<TypingEvent> typingEvents)
+        {
+            this.sourceDocument.push(typingEvents);
+        }
+
+        @Override
+        public Queue<TypingEvent> events()
+        {
+            return this.sourceDocument.events();
+        }
+
+        @Override
+        public Queue<TypingEvent> eventsSince(long time)
+        {
+            return this.sourceDocument.eventsSince(time);
+        }
+
+        @Override
+        public long lastUpdateTime()
+        {
+            return currentTime;
+        }
+
+        @Override
+        public void clearAll()
+        {
+            this.sourceDocument.clearAll();
+        }
+    }
+
+    public class PseudoClient implements ICodeLocation
+    {
+        SourceDocument sourceDocument;
+        long lastUpdateTime;
+        SDDemoPanel panel;
+        private Timer timer = new Timer();
+        private long delay = 0;
+        private long period = 100;
+        int id;
+
+        public PseudoClient(final SDDemoPanel panel, final int id,
+                SourceDocument sourceDocument)
+        {
+            this.sourceDocument = sourceDocument;
+            this.panel = panel;
+            this.timer.scheduleAtFixedRate(new TimerTask()
+            {
+                public void run()
+                {
+                    /*
+                     * try { panel.updateText(sourceDocument.toString()); }//
+                     * System.out.println(id + " : " + sourceDocument); catch
+                     * (Exception e) { e.printStackTrace(); System.exit(1); }
+                     */
+
+                }
+            }, this.delay, this.period);
+            this.id = id;
+
+            this.panel.eta.addActionListener(new ActionListener()
+            {
+
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    Queue<TypingEvent> outgoingEvents = new LinkedList<TypingEvent>();
+                    TypingEvent te = (TypingEvent) e.getSource();
+                    outgoingEvents.add(te);
+                    System.out.println("push to server: " + te);
+                    server.push(outgoingEvents);
+                }
+
+            });
+        }
+
+        @Override
+        public void push(Queue<TypingEvent> typingEvents)
+        {
+            if (typingEvents.size() > 0)
+            {
+                TypingEvent[] events = new TypingEvent[typingEvents.size()];
+                typingEvents.toArray(events);
+                this.sourceDocument.push(typingEvents);
+
+                try
+                {
+                    this.panel.eta.updateText();
+                    // panel.updateText(sourceDocument.toString());
+                }// System.out.println(id + " : " + sourceDocument);
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+
+                for (TypingEvent te : events)
+                {
+
+                    /*
+                     * switch (te.mode) { case insert: if (te.position >=
+                     * panel.eta.getCaretPosition()) panel.eta.moveRight();
+                     * break; case overwrite: if (te.position >=
+                     * panel.eta.getCaretPosition()) panel.eta.moveRight();
+                     * break; case backspace: if (te.position >=
+                     * panel.eta.getCaretPosition()) panel.eta.moveLeft();
+                     * break; }
+                     */
+                }
+            }
+        }
+
+        @Override
+        public Queue<TypingEvent> events()
+        {
+            return this.sourceDocument.events();
+        }
+
+        @Override
+        public Queue<TypingEvent> eventsSince(long time)
+        {
+            return this.sourceDocument.eventsSince(time);
+        }
+
+        @Override
+        public long lastUpdateTime()
+        {
+            return this.sourceDocument.lastUpdateTime();
+        }
+
+        @Override
+        public void clearAll()
+        {
+            this.sourceDocument.clearAll();
+        }
+
+    }
+
+    public class SDDemoPanel extends JPanel
+    {
+        EditorTypingArea eta;
+        ICodeLocation server;
+        ICodeLocation client;
+        int id;
+
+        public SDDemoPanel(JFrame w, Dimension size,
+                final ICodeLocation server, final Timer timer, final int id,
+                final EditorTypingArea eta)
+        {
+            this.id = id;
+            this.setSize(size);
+            this.eta = eta;
+            this.eta.setPreferredSize(size);
+            this.add(this.eta);
+            this.server = server;
+            w.addKeyListener(new KeyListener()
+            {
+
+                @Override
+                public void keyPressed(KeyEvent ke)
+                {
+                    switch (ke.getKeyCode())
+                    {
+                    case KeyEvent.VK_LEFT:
+                        eta.moveLeft();
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        eta.moveRight();
+                        break;
+                    case KeyEvent.VK_UP:
+                        eta.moveUp();
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        eta.moveDown();
+                        break;
+                    }
+                }
+
+                @Override
+                public void keyReleased(KeyEvent ke)
+                {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void keyTyped(KeyEvent ke)
+                {
+                    try
+                    {
+
+                        Queue<TypingEvent> outgoingEvents = new LinkedList<TypingEvent>();
+                        // System.out.println(server.lastUpdateTime());
+                        TypingEventMode mode = TypingEventMode.insert;
+                        switch (ke.getKeyChar())
+                        {
+                        case '\u0008':
+                        {
+                            if (eta.getCaretPosition() > 0
+                                    && !eta.currentPositionLocked(-1))
+                            {
+                                mode = TypingEventMode.backspace;
+                            }
+                            else
+                                return;
+                        }
+                            break;
+                        // case '\u0027':
+                        // tryCaretPosition(position + 1);
+                        // break;
+                        default:
+                        {
+                            if (eta.currentPositionLocked(0))
+                                return;
+                        }
+                        }
+
+                        long t = System.currentTimeMillis();
+
+                        TypingEvent te = new TypingEvent(t, mode, eta
+                                .getCaretPosition(), 1, String.valueOf(ke
+                                .getKeyChar()), "Demo User " + id);
+                        System.out.println("push to server: " + te);
+                        outgoingEvents.add(te);
+
+                        Queue<TypingEvent> internal = new LinkedList<TypingEvent>(
+                                outgoingEvents);
+                        server.push(outgoingEvents);
+                        client.push(internal);
+
+                        switch (mode)
+                        {
+                        case insert:
+                            eta.moveRight();
+                            break;
+                        case overwrite:
+                            eta.moveRight();
+                            break;
+                        case backspace:
+                            eta.moveLeft();
+                            break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                        timer.cancel();
+                        System.exit(1);
+                    }
+                }
+            });
+        }
+
+        public void setClient(ICodeLocation client)
+        {
+            this.client = client;
+        }
+    }
+}
