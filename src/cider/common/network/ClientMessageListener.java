8345c49740dc2a558fe08261343b5e0e5776e7d2
Pikmindoctor <Pikmindoctor@gmail.com>
2011-03-07 18:58:18 +0000

    Line locking - got rid of the stray bit of line lock at the end when you unlock a line

---
+++ src/cider/common/network/ClientMessageListener.java
@@ -0,0 +1,157 @@
+package cider.common.network;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.util.StringUtils;
+
+import cider.common.processes.Profile;
+
+/**
+ * This class waits for a message to be received by the client on its chat
+ * session with the bot.
+ * 
+ * @author Andrew + Lawrence
+ * 
+ */
+
+public class ClientMessageListener implements MessageListener, ActionListener
+{
+    private Client client;
+
+    public ClientMessageListener(Client client)
+    {
+        this.client = client;
+    }
+
+    @Override
+    public void processMessage(Chat chat, Message message)
+    {
+        String body = new String(StringUtils.decodeBase64(message.getBody()));
+        if (body.startsWith("quit"))
+        {
+            JOptionPane
+                    .showMessageDialog(
+                            new JPanel(),
+                            "Error: Someone is already running a CIDER client with your username, disconnecting and quitting.");
+            client.disconnect();
+            System.exit(1);
+        }
+        else if (body.equals("yes i am online"))
+        {
+        	client.botIsOnline = true;
+        }
+        else if (body.equals("notfound"))
+        {
+            client.profileFound = false;
+        }
+        else if (body.startsWith("usercolour:"))
+        {
+            String[] split = body.split(" ");
+            Color c = new Color(Integer.parseInt(split[1]),
+                    Integer.parseInt(split[2]), Integer.parseInt(split[3]));
+            client.incomingColour = c;
+        }
+        else if (body.startsWith("PROFILE* "))
+        {
+            System.out.println(body);
+            if (client.profile == null)
+            {
+                client.profile = new Profile(client.getUsername(), client);
+            }
+            client.profileFound = true;
+            String[] splitLine = body.split(" ");
+            if (splitLine[1].equals("chars:"))
+            {
+                client.profile.setChars(Integer.parseInt(splitLine[2]));
+            }
+            else if (splitLine[1].equals("timespent:"))
+            {
+                client.profile.setTime(Long.parseLong(splitLine[2]));
+            }
+            else if (splitLine[1].equals("lastonline:"))
+            {
+                String newsplit = body.substring(21);
+                client.profile.setLastOnline(newsplit);
+            }
+            else if (splitLine[1].equals("colour:"))
+            {
+                client.profile.setColour(Integer.parseInt(splitLine[2]),
+                        Integer.parseInt(splitLine[3]),
+                        Integer.parseInt(splitLine[4]));
+            }
+            else
+            {
+                client.profile.uname = body.substring(9);
+            }
+        }
+        else if (body.startsWith("PROFILE$ "))
+        {
+            if (client.notMyProfile == null)
+            {
+                client.notMyProfile = new Profile("notme", client);
+            }
+            client.profileFound = true;
+            String[] splitLine = body.split(" ");
+            if (splitLine[1].equals("chars:"))
+            {
+                client.notMyProfile.setChars(Integer.parseInt(splitLine[2]));
+            }
+            else if (splitLine[1].equals("timespent:"))
+            {
+                client.notMyProfile.setTime(Long.parseLong(splitLine[2]));
+            }
+            else if (splitLine[1].equals("lastonline:"))
+            {
+                String newsplit = body.substring(21);
+                client.notMyProfile.setLastOnline(newsplit);
+            }
+            else if (splitLine[1].equals("colour:"))
+            {
+                client.notMyProfile.setColour(Integer.parseInt(splitLine[2]),
+                        Integer.parseInt(splitLine[3]),
+                        Integer.parseInt(splitLine[4]));
+            }
+            else
+            {
+                client.notMyProfile.uname = body.substring(9);
+            }
+        }
+        else if (body.startsWith("timeReply("))
+        {
+            String str = body.split("\\(")[1];
+            str = str.split("\\)")[0];
+            String[] args = str.split(",");
+            long sentTime = Long.parseLong(args[0]);
+            long currentTime = System.currentTimeMillis()
+                    + this.client.getClockOffset();
+            long halfLatency = (currentTime - sentTime) / 2;
+            long delta = currentTime - Long.parseLong(args[1]) + halfLatency;
+
+            if (this.client.getClockOffset() == 0)
+                this.client.setTimeDelta(delta);
+
+            this.client.addTimeDeltaSample(delta);
+        }
+        else
+            this.client.processDocumentMessages(body);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        /*
+         * try { this.client.pullEventsFromBot(this.client.getLastUpdate()); }
+         * catch (Exception e) { e.printStackTrace();
+         * JOptionPane.showMessageDialog(null, "Cannot pull events: " +
+         * e.getMessage()); System.exit(1); }
+         */
+    }
+}
---
+++ src/cider/common/processes/TypingEvent.java
@@ -0,0 +1,126 @@
+package cider.common.processes;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Lawrence
+ * 
+ */
+public class TypingEvent
+{
+    public String lockingGroup = null;
+    public final TypingEventMode mode;
+    public final long time;
+    public final int position;
+    public final int length;
+    public final String text;
+    public final String owner;
+
+    public TypingEvent(long time, final TypingEventMode mode, int position,
+            int length, String text, String owner, String lockingGroup)
+    {
+        this.time = time;
+        this.mode = mode;
+        this.position = position;
+        this.text = text;
+        this.length = length;
+        this.owner = owner;
+        this.lockingGroup = lockingGroup;
+    }
+
+    public TypingEvent(TypingEvent typingEvent, long time, int position,
+            TypingEventMode mode)
+    {
+        this.time = time;
+        this.position = position;
+        this.mode = mode;
+        this.lockingGroup = typingEvent.lockingGroup;
+        this.length = typingEvent.length;
+        this.text = typingEvent.text;
+        this.owner = typingEvent.owner;
+
+    }
+
+    public TypingEvent(TypingEvent typingEvent, long time, String text)
+    {
+        this.time = time;
+        this.position = typingEvent.position;
+        this.mode = typingEvent.mode;
+        this.lockingGroup = typingEvent.lockingGroup;
+        this.text = text;
+        this.length = text.length();
+        this.owner = typingEvent.owner;
+    }
+
+    public TypingEvent(String str)
+    {
+        String[] split = str.split("~");
+        // it's possible the user will want
+        // to use ~ so we need some way of
+        // telling the difference.
+        try
+        {
+            this.mode = TypingEventMode.values()[Integer.parseInt(split[0])];
+            this.text = split[1];
+            this.position = Integer.parseInt(split[2], 35);
+            this.length = Integer.parseInt(split[3]);
+            this.time = Long.parseLong(split[4], 35);
+            this.owner = split[5];
+            if (split.length == 7)
+                this.lockingGroup = split[6];
+            else
+                this.lockingGroup = null;
+        }
+        catch (Exception e)
+        {
+            throw new Error("Failed to parse " + str + ". " + e.getMessage());
+        }
+    }
+
+    public String pack()
+    {
+        return this.mode.ordinal() + "~" + this.text + "~"
+                + Integer.toString(this.position, 35) + "~" + this.length + "~"
+                + Long.toString(this.time, 35) + "~" + this.owner
+                + (this.lockingGroup == null ? "" : "~" + this.lockingGroup);
+    }
+
+    public ArrayList<TypingEvent> explode()
+    {
+        ArrayList<TypingEvent> particles = new ArrayList<TypingEvent>();
+        if (this.mode == TypingEventMode.lockRegion
+                || this.mode == TypingEventMode.unlockRegion)
+            particles.add(this);
+        else
+        {
+            char[] chrs = this.text.toCharArray();
+            long t = this.time;
+
+            for (char chr : chrs)
+                particles.add(new TypingEvent(this, t++, "" + chr));
+        }
+        return particles;
+    }
+
+    public void setLockingGroup(String lockingGroup)
+    {
+        this.lockingGroup = lockingGroup;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "time " + this.time + "\t" + this.position + "\t" + this.length
+                + "\t" + this.mode.toString() + "\t" + this.text + "\t"
+                + this.lockingGroup;
+    }
+
+    public boolean existsIn(TypingEvent[] tes)
+    {
+        for (TypingEvent te : tes)
+            if (this.time == te.time)
+                return true;
+        return false;
+    }
+}
