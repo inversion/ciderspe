f2792eada68d7461b327f256fe0ada2c7fc1a5dd
Pikmindoctor <Pikmindoctor@gmail.com>
2011-04-07 18:56:52 +0100

    Merge

---
+++ src/cider/client/gui/TimeRegionBrowser.java
@@ -0,0 +1,173 @@
+package cider.client.gui;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+import javax.swing.JPanel;
+
+import cider.common.processes.TimeBorder;
+import cider.common.processes.TimeBorderList;
+import cider.common.processes.TimeRegion;
+
+public class TimeRegionBrowser extends JPanel implements MouseListener,
+        MouseMotionListener
+{
+    TimeBorderList tbl;
+    PriorityQueue<Long> borderTimes;
+    double scale = 0.1;
+    long eyePosition = 500;
+    boolean movingEye = false;
+    long latestTime = 0;
+    ArrayList<ActionListener> actionListeners = new ArrayList<ActionListener>();
+
+    public TimeRegionBrowser(TimeBorderList tbl)
+    {
+        this.tbl = tbl;
+        this.borderTimes = tbl.borderTimes();
+        this.setPreferredSize(new Dimension(128, 128));
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+    }
+
+    public void updateBorderTimes()
+    {
+        this.borderTimes = this.tbl.borderTimes();
+    }
+
+    public void setScale(double scale)
+    {
+        this.scale = scale;
+    }
+
+    public double getScale()
+    {
+        return this.scale;
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        g.clearRect(0, 0, this.getWidth(), this.getHeight());
+        int x = 16;
+        int y = 0;
+        int prevY = 0;
+        int width = this.getWidth() - 32;
+        TimeBorder timeBorder;
+        g.setColor(Color.BLACK);
+        g.drawRect(x - 1, 1, width + 1, this.getHeight() - 1);
+
+        for (long t : this.borderTimes)
+        {
+            this.latestTime = t;
+            timeBorder = this.tbl.getBorder(t);
+            y = (int) (((double) t) * this.scale);
+            g.setColor(Color.BLACK);
+            g.drawLine(x, y, x + width - 1, y);
+
+            if (timeBorder.fullSet)
+                g.setColor(new Color(128, 128, 255));
+            else
+                g.setColor(Color.LIGHT_GRAY);
+
+            g.fillRect(x, prevY + 1, width, y - prevY - 1);
+            prevY = y;
+        }
+
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y + 1, width, this.getHeight() - y);
+        this.paintEye(g, x + width);
+    }
+
+    public void paintEye(Graphics g, int x)
+    {
+        g.setColor(Color.BLACK);
+        g.drawOval(x + 1, (int) (this.eyePosition * this.scale) - 4, 14, 8);
+        g.setColor(Color.cyan);
+        g.fillOval(x + 5, (int) (this.eyePosition * this.scale) - 4, 6, 6);
+        g.setColor(Color.BLACK);
+        g.drawOval(x + 5, (int) (this.eyePosition * this.scale) - 4, 6, 6);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent me)
+    {
+        if (this.movingEye)
+        {
+            this.eyePosition = (long) (me.getY() / this.scale);
+            if (this.eyePosition < 0)
+                this.eyePosition = 0;
+            if (this.eyePosition > this.latestTime)
+                this.eyePosition = this.latestTime;
+            this.repaint();
+            this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+            for (ActionListener al : this.actionListeners)
+                al.actionPerformed(new ActionEvent(this.eyePosition, 0,
+                        "Eye Moved"));
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me)
+    {
+        if (me.getX() > this.getWidth() - 16)
+        {
+            this.movingEye = true;
+            this.repaint();
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0)
+    {
+        this.movingEye = false;
+        this.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+    }
+
+    public void addActionListener(ActionListener al)
+    {
+        this.actionListeners.add(al);
+    }
+
+    public TimeRegion getCurrentRegion()
+    {
+        return this.tbl.regionThatCovers(this.eyePosition);
+    }
+}
---
+++ src/cider/common/network/bot/BotMessageListener.java
@@ -0,0 +1,395 @@
+/**
+ *  CIDER - Collaborative Integrated Development EnviRonment
+    Copyright (C) 2011  Andrew Moss
+                        Lawrence Watkiss
+                        Jonathan Bannister
+                        Alex Sheppard
+                        Miles Taylor
+                        Ashley Woodman
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cider.common.network.bot;
+
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.util.Base64;
+import org.jivesoftware.smack.util.StringUtils;
+
+import cider.common.processes.LiveFolder;
+import cider.common.processes.SourceDocument;
+import cider.common.processes.TypingEvent;
+
+/**
+ * This class waits for a message to be received on a chat session and then
+ * responds to messages accordingly.
+ * 
+ * 
+ * @author Andrew
+ * 
+ */
+
+public class BotMessageListener implements MessageListener
+{
+    public static boolean DEBUG = true;
+    
+    private BotChatListener source;
+    private String name;
+    private Bot bot;
+
+
+    public BotMessageListener(BotChatListener source, String name, Bot bot)
+    {
+        this.source = source;
+        this.name = name;
+        this.bot = bot;
+        // System.out.println(this.liveFolder.xml(""));
+    }
+    
+    /**
+     * Goes through a path and finds the corresponding live folder object.
+     * 
+     * @param path The remainder of the path we are finding.
+     * @param current The current LiveFolder we are in.
+     * @return The LiveFolder, or null if it wasn't found.
+     * 
+     * @author Andrew
+     */
+    private LiveFolder findFolder( String path, LiveFolder current )
+    {
+        String part;
+        // Trim trailing slash
+        if( path.endsWith( "\\" ) )
+            path = path.substring( 0, path.length()-1 );
+        
+        while( path.length() > 0 )
+        {
+            if( path.indexOf("\\") == -1 )
+            {
+                System.out.println("Returning " + path);
+                return current.getFolder( path );
+            }
+            else
+            {
+                part = path.substring( 0, path.indexOf( "\\" ) );
+                current = current.getFolder( part );
+                path = path.substring( part.length()+1 );
+            }
+        }
+        
+        return current;
+    }
+
+    @Override
+    public void processMessage(Chat chat, Message message)
+    {
+        String body = null;
+        body = new String(Base64.decode(message.getBody()));
+        // TODO: XML-ize this and get filelist??
+        if (body.startsWith("quit"))
+            source.endSession(name);
+        else if ( body.startsWith( "createDocument=" ) )
+        {
+            String serial = body.substring( "createDocument=".length() );
+            ByteArrayInputStream bis = new ByteArrayInputStream( serial.getBytes() );
+            ObjectInputStream os = null;
+            SourceDocument doc = null;
+            
+            // A bit hackish, but using subject as path to create document in
+            String path = new String( Base64.decode( message.getSubject() ) );
+            
+            try
+            {
+                os = new ObjectInputStream( bis );
+                doc = (SourceDocument) os.readObject();
+                
+                if( DEBUG )
+                    System.out.println( "Creating document " + doc.name + " in " + findFolder( path, bot.getRootFolder() ).name );
+                
+                findFolder( path, bot.getRootFolder() ).addDocument( doc );             
+                
+                // TODO: Duplicate code from below
+                String xml = this.bot.getRootFolder().xml("");
+                bot.chatroom.sendMessage(StringUtils.encodeBase64("filelist=" + xml));   
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (ClassNotFoundException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (XMPPException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }        
+        }
+        else if (body.startsWith("requestusercolour"))
+        {
+            String[] split = body.split(" ");
+            Color yaycolour = bot.colours.get(split[2]);
+            try
+            {
+                source.chats.get(split[1]).sendMessage(
+                        StringUtils.encodeBase64("usercolour: "
+                                + yaycolour.getRed() + " "
+                                + yaycolour.getGreen() + " "
+                                + yaycolour.getBlue()));
+            }
+            catch (XMPPException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else if (body.startsWith("userprofile:"))
+        {
+            String[] splitProfile = body.split("  ");
+            File f = new File("profile_" + splitProfile[1] + ".txt");
+            try
+            {
+                f.createNewFile();
+                FileWriter fw = new FileWriter(f);
+                BufferedWriter out = new BufferedWriter(fw);
+                String s = splitProfile[1] + "\n" + splitProfile[2] + "\n"
+                        + splitProfile[3] + "\n" + splitProfile[4] + "\n"
+                        + splitProfile[5];
+                System.out
+                        .println("**********RECEIVED PROFILE**********\n" + s);
+                System.out.println("************************************");
+                out.write(s);
+                out.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+        else if (body.equals("are you online mr bot"))
+        {
+            System.out.println("Online query received");
+            try
+            {
+                chat.sendMessage(StringUtils.encodeBase64("yes i am online"));
+            }
+            catch (XMPPException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else if (body.startsWith("requestprofile"))
+        {
+            String[] splitbody = body.split(" ");
+            System.out.println(body);
+            boolean notme = false;
+            for (int i = 0; i < splitbody.length; i++)
+            {
+                if (splitbody[i].equals("notme"))
+                    notme = true;
+            }
+            try
+            {
+                File f = new File("profile_" + splitbody[1] + ".txt");
+                if (f.exists())
+                {
+                    FileInputStream fis = new FileInputStream(f);
+                    DataInputStream dis = new DataInputStream(fis);
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(dis));
+                    String line;
+                    System.out.println("Reading profile, sending:\n");
+                    while ((line = br.readLine()) != null)
+                    {
+                        System.out.println(line);
+                        // Send profile file to client
+                        if (notme)
+                            chat.sendMessage(StringUtils
+                                    .encodeBase64("PROFILE$ " + line));
+                        else
+                            source.chats.get(splitbody[1]).sendMessage(
+                                    StringUtils
+                                            .encodeBase64("PROFILE* " + line));
+                    }
+                }
+                else
+                {
+                    // Send message indicating no profile was found
+                    source.chats.get(splitbody[1]).sendMessage(
+                            StringUtils.encodeBase64("notfound"));
+                    System.out.println("Profile not found!");
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+                System.err
+                        .println("Error: IO error when retrieving profile for "
+                                + splitbody[1]);
+            }
+            catch (XMPPException e)
+            {
+                e.printStackTrace();
+                System.err
+                        .println("XMPP Exception whilst retrieving profile. Error message: "
+                                + e.getMessage());
+            }
+        }
+        else if (body.equals("getfilelist"))
+        {
+            try
+            {
+                String xml = this.bot.getRootFolder().xml("");
+                chat.sendMessage(StringUtils.encodeBase64("filelist=" + xml));
+            }
+            catch (XMPPException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        // This part is still important for when a file is opened
+        else if (body.startsWith("pullEvents("))
+        {
+            String str = body.split("\\(")[1];
+            str = str.split("\\)")[0];
+            String[] args = str.split(",");
+            String dest = args[0];
+            long startTime = Long.parseLong(args[1]);
+
+            if (args.length == 2)
+                this.pushBack(chat, dest, this.bot.getRootFolder().path(dest)
+                        .eventsSince(startTime), false);
+            else
+            {
+                long endTime = Long.parseLong(args[2]);
+                this.pushBack(chat, dest, this.bot.getRootFolder().path(dest)
+                        .eventsBetween(startTime, endTime), args[3].equals("t"));
+            }
+
+        }
+        else if (body.startsWith("pullSimplifiedEvents("))
+        {
+            String str = body.split("\\(")[1];
+            str = str.split("\\)")[0];
+            String[] args = str.split(",");
+            String dest = args[0];
+            long t = Long.parseLong(args[1]);
+            this.pushBack(chat, dest, this.bot.getRootFolder().path(dest)
+                    .simplified(t).events(), false);
+        }
+        else if (body.startsWith("You play 2 hours to die like this?"))
+        {
+            Toolkit.getDefaultToolkit().beep();
+            System.err
+                    .println("The bot has been shut down by a backdoor routine");
+            System.exit(1);
+        }
+        else if (body.startsWith("timeRequest("))
+        {
+            // 2: Upon receipt by server, server stamps server-time and returns
+            try
+            {
+                String sentTime = body.split("\\(")[1];
+                sentTime = sentTime.split("\\)")[0];
+                chat.sendMessage(StringUtils.encodeBase64("timeReply("
+                        + sentTime + "," + System.currentTimeMillis() + ")"));
+            }
+            catch (XMPPException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        // probably not useful anymore \/
+        else if (body.startsWith("pushto("))
+        {
+            String[] instructions = body.split("\\) \\n");
+            for (String instruction : instructions)
+            {
+                String[] preAndAfter = instruction.split("\\) ");
+                String[] pre = preAndAfter[0].split("\\(");
+                String dest = pre[1];
+                dest = dest.replace("root\\", "");
+                Queue<TypingEvent> typingEvents = new LinkedList<TypingEvent>();
+                typingEvents.add(new TypingEvent(preAndAfter[1]));
+                System.out.println("Push " + preAndAfter[1] + " to " + dest);
+                this.bot.getRootFolder().path(dest).push(typingEvents);
+            }
+        }
+    }
+
+    private void pushBack(Chat chat, String path, Queue<TypingEvent> queue,
+            boolean stopDiversion)
+    {
+        String instructions = "";
+        if (queue.size() == 0)
+            instructions += "isblank(" + path + ")";
+        else
+        {
+            instructions += "pushto(" + path + ") ";
+            for (TypingEvent te : queue)
+                instructions += te.pack() + "%%";
+        }
+
+        if (stopDiversion)
+            instructions += "stopdiversion";
+
+        try
+        {
+            chat.sendMessage(StringUtils.encodeBase64(instructions));
+        }
+        catch (XMPPException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * FIXME: Unused method!
+     * 
+     */
+    /*
+     * private void pushBack(Chat chat, Queue<LocalisedTypingEvents> events) {
+     * String instructions = ""; for (LocalisedTypingEvents ltes : events) for
+     * (TypingEvent te : ltes.typingEvents) instructions += "pushto(" +
+     * ltes.path + ") " + te.pack() + ">"; try {
+     * chat.sendMessage(StringUtils.encodeBase64(instructions)); } catch
+     * (XMPPException e) { // TODO Auto-generated catch block
+     * e.printStackTrace(); } }
+     */
+}
