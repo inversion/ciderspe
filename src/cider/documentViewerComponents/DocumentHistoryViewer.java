7e234f5017af97a186bc376dc6430974d44a1522
mossy1278 <mossy1278@gmail.com>
2011-04-13 16:13:08 +0100

    Paste feature working in editor.

---
+++ src/cider/documentViewerComponents/DocumentHistoryViewer.java
@@ -0,0 +1,48 @@
+/**
+ *  CIDER - Collaborative Integrated Development EnviRonment
+    Copyright (C) 2011  Andrew Moss
+                        Lawrence Watkiss
+                        Jonathan Bannister
+                        Alex Sheppard
+                        Miles Taylor
+                        Ashley Woodman
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cider.documentViewerComponents;
+
+import cider.common.processes.SourceDocument;
+import cider.common.processes.TimeRegion;
+
+/**
+ * The DocumentHistoryViewer is a type of SourceDocumentViewer that is read only
+ * and you cannot see or move any carets.
+ * 
+ * @author Lawrence
+ * 
+ */
+public class DocumentHistoryViewer extends SourceDocumentViewer
+{
+    public DocumentHistoryViewer(SourceDocument sourceDocument)
+    {
+        super(sourceDocument);
+    }
+
+    public void useEventsFrom(TimeRegion currentRegion)
+    {
+        this.doc.clearAll();
+        this.doc.addEvents(currentRegion.end.typingEvents);
+    }
+}
