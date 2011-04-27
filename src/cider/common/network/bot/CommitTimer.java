d25c54eddbc641961ae5803789de86f16242b85f
milesvt <milesvt@localhost>
2011-04-27 14:52:52 +0100

    Error message displays when port isn't a number. Also opens new instance of loginUI on errors rather than quitting entirely.

---
+++ src/cider/common/network/bot/CommitTimer.java
@@ -0,0 +1,42 @@
+package cider.common.network.bot;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CommitTimer
+{
+    private Timer timer = new Timer();
+    private Bot bot;
+    
+    public CommitTimer( Bot bot )
+    {
+        this.bot = bot;
+        timer.scheduleAtFixedRate( commitToDisk, 60000, 60000 );
+    }
+    
+    protected void stopTimer()
+    {
+        timer.cancel();
+    }
+    
+    TimerTask commitToDisk = new TimerTask()
+    {
+        @Override
+        public void run()
+        {
+            bot.writeUpdatedDocs();
+            bot.writeUpdatedProfiles();
+            try
+            {
+                bot.writeChatHistory();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    };
+
+}
