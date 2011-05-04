75a2abcf2a6c1a36164c91db15f5926d24412b1c
mossy1278 <mossy1278@gmail.com>
2011-05-04 13:41:38 +0100

    disabling history stuff

--- src/cider/client/gui/ETASourceEditorPane.java
+++ src/cider/client/gui/ETASourceEditorPane.java
@@ -100,8 +100,9 @@ public class ETASourceEditorPane extends JScrollPane
         this.setWheelScrollingEnabled(false);
         this.addMouseWheelListener(this.newMouseWheelListener());
         this.client.getParent().w.addWindowListener(this.newWindowListener());
-        SiHistoryFiles.markDocumentOpening(path, System.currentTimeMillis()
-                + client.getClockOffset());
+        // FIXME: Caused exception
+//        SiHistoryFiles.markDocumentOpening(path, System.currentTimeMillis()
+//                + client.getClockOffset());
     }
 
     private MouseWheelListener newMouseWheelListener()
@@ -138,15 +139,15 @@ public class ETASourceEditorPane extends JScrollPane
             @Override
             public void windowClosed(WindowEvent arg0)
             {
-                SiHistoryFiles.markDocumentClosing(path,  System.currentTimeMillis()
-                        + client.getClockOffset());
+                //SiHistoryFiles.markDocumentClosing(path,  System.currentTimeMillis()
+                //        + client.getClockOffset());
             }
 
             @Override
             public void windowClosing(WindowEvent arg0)
             {
-                SiHistoryFiles.markDocumentClosing(path,  System.currentTimeMillis()
-                        + client.getClockOffset());
+                //SiHistoryFiles.markDocumentClosing(path,  System.currentTimeMillis()
+                //        + client.getClockOffset());
             }
 
             @Override
@@ -607,7 +608,7 @@ public class ETASourceEditorPane extends JScrollPane
                                 }
                                 
                                 
-                                SiHistoryFiles.saveEvents(toFile, client.getCurrentDocumentID().path);
+//                                SiHistoryFiles.saveEvents(toFile, client.getCurrentDocumentID().path);
 
                                 eta.getSourceDocument().push(internal);
                                 client.broadcastTypingEvents(outgoingEvents,
