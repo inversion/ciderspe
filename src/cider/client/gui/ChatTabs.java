8385c31744e1ceaa135567a73d3e601d4cfd876e
Pikmindoctor <Pikmindoctor@gmail.com>
2011-05-06 23:18:05 +0100

    Merge

---
+++ src/cider/client/gui/ChatTabs.java
@@ -0,0 +1,104 @@
+package cider.client.gui;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+import javax.swing.JTabbedPane;
+import javax.swing.Timer;
+
+/**
+ * Extension of JTabbedPane to include flashing functionality
+ * 
+ * @author Alex, Andrew
+ *
+ */
+
+@SuppressWarnings("serial")
+public class ChatTabs extends JTabbedPane
+{
+    private HashMap<String,TabFlashListener> usersToTabs = new HashMap<String,TabFlashListener>();
+    
+    public void tabflash( String name )
+    {
+        // Don't flash the tab if it's the currently selected one
+        if( this.getSelectedIndex() == this.indexOfTab( name ) )
+            return;
+               
+        TabFlashListener tabflash = new TabFlashListener( this, this.indexOfTab( name ) );
+        usersToTabs.put( name, tabflash );
+        tabflash.start();
+    }
+
+    public void tabflashstop( String name )
+    {
+        if( usersToTabs.containsKey( name ) )
+            usersToTabs.get( name ).stopflash();
+    }
+
+    public class TabFlashListener implements ActionListener
+    {
+        private Color background;
+        private Color foreground;
+        private Color oldBackground;
+        private Color oldForeground;
+        private int requiredTabIndex;
+        private boolean flashon = false;
+        private Timer timer = new Timer(700, this);
+
+        public TabFlashListener( ChatTabs receiveTabs, int requiredTabIndex )
+        {
+            this.requiredTabIndex = requiredTabIndex;
+            this.oldForeground = receiveTabs.getForeground();
+            this.oldBackground = receiveTabs.getBackground();
+            this.foreground = Color.BLACK;
+            this.background = Color.ORANGE;
+        }
+
+        public void start()
+        {
+            timer.start();
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            flash(flashon);
+            flashon = !flashon;
+        }
+
+        public void flash(boolean flashon)
+        {
+            if (flashon)
+            {
+                if (foreground != null)
+                {
+                    setForegroundAt(requiredTabIndex, foreground);
+                }
+                if (background != null)
+                {
+                    setBackgroundAt(requiredTabIndex, background);
+                }
+            }
+            else
+            {
+                if (oldForeground != null)
+                {
+                    setForegroundAt(requiredTabIndex, oldForeground);
+                }
+                if (oldBackground != null)
+                {
+                    setBackgroundAt(requiredTabIndex, oldBackground);
+                }
+            }
+            repaint();
+        }
+
+        public void stopflash()
+        {
+            timer.stop();
+            setForegroundAt(requiredTabIndex, oldForeground);
+            setBackgroundAt(requiredTabIndex, oldBackground);
+        }
+    }
+}
---
+++ src/cider/client/gui/DirectoryViewSelectionListener.java
@@ -0,0 +1,70 @@
+/**
+ *  CIDER - Collaborative Integrated Development EnviRonment
+    Copyright (C) 2011  Andrew Moss
+                        Lawrence Watkiss
+                        Jonathan Bannister
+                        Alex Sheppard
+                        Miles Taylor
+                        Ashley Woodman
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package cider.client.gui;
+
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import cider.common.network.client.Client;
+
+/**
+ * Listens for when a tree node is selected and if one is it gets a file from
+ * the server and opens it in a new tab.
+ * 
+ * TODO: Type checking, error checking TODO: Commenting to explain functionality
+ * 
+ * @author Andrew
+ * 
+ */
+
+public class DirectoryViewSelectionListener implements TreeSelectionListener
+{
+
+    private JTree tree;
+    private Client client;
+
+    DirectoryViewSelectionListener(JTree tree, Client client)
+    {
+        this.tree = tree;
+        this.client = client;
+    }
+
+    @Override
+    public void valueChanged(TreeSelectionEvent e)
+    {
+        // TODO Auto-generated method stub
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree
+                .getLastSelectedPathComponent();
+
+        // Get the file and let the client message listener make the new tab
+        // If the path is null it's a dir
+        if (node != null && node.isLeaf())
+        {
+            client.openTabFor(node.getUserObjectPath());
+            // client.directKeyboardInputTo(node.getUserObjectPath());
+        }
+    }
+}
